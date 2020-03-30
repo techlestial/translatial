6d15599259267aaf0bca9a5974dd18b2d1c2a57c
m3yevn <mmm.myintmyat@gmail.com>
2020-03-30 22:05:06 +0800

    🍔 Implement translate mutation with multiple languages

--- models/models.py
+++ models/models.py
@@ -1,6 +1,11 @@
-from graphene import ObjectType, String
+from graphene import ObjectType, String, Field
 
 
 class Language(ObjectType):
     name = String()
     lang_code = String()
+
+class Translation(ObjectType):
+    target_language = Field(Language)
+    content = String()
+    pronunciation = String()
--- mutation/translate.py
+++ mutation/translate.py
@@ -3,16 +3,18 @@ import json
 from graphene import ObjectType, List, String, Mutation
 from libs.utils.jsonutils import json2obj
 from libs.utils.commonutils import _init_google_translate
+from models.models import Translation
 
 
 class TranslateWords(Mutation):
     class Arguments:
         content = String(required=True)
         target_languages = List(String,required=True)
-        source_language = String()
-    result = String()
+    result = List(Translation)
     def mutate(self, info, content,target_languages, **kargs):
         translator = _init_google_translate()
-        response = translator.translate(content,dest=target_languages[0])
-        print(response)
-        return TranslateWords(result=response.text)
+        result_list = []
+        for target in target_languages:
+            response = translator.translate(content,dest=target)
+            result_list.append(Translation(target_language=target,content=response.text,pronunciation=response.pronunciation if response.pronunciation else 'N.A'))
+        return TranslateWords(result=result_list)
